a6252e391114725c08ff8e95c7ce52e4a922f0cf
ignasftw <32689847+ignasftw@users.noreply.github.com>
2020-02-10 13:12:03 +0000

    To-Do rework plan

--- COMP3304Session1/FishyNotes.cs
+++ COMP3304Session1/FishyNotes.cs
@@ -13,7 +13,7 @@ namespace COMP3304Session1
 {
     public partial class FishyNotes : Form, IFishyNotes
     {
-
+        //TO-DO change this so Notes would be contained separately from Data
         //Declaire a Container which will store data of _notes, call it '_noteData'
         INotes _noteData = new NoteData();
 
@@ -22,16 +22,30 @@ namespace COMP3304Session1
             InitializeComponent();
         }
 
-        /*
-         * Adds a new 'FishyNote'
-         * After the button has been pressed
-         */
+        /// <summary>
+        /// Adds a new 'FishyNote'
+        /// After the button has been pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddNote_Click(object sender, EventArgs e)
         {
             //Asking NoteData to create a Note
             _noteData.AddNote(_noteData);
         }
 
+        public void RemoveNote(int id)
+        {
+            _noteData.RemoveNote(id);
+        }
+
+        /*
+         
+             This should be done in 
+             _noteData
+             
+             
+             */
         /*
          * Adds 'FishyNote''s text into a dictionary
          * int id - id of the FishyNote
@@ -42,6 +56,14 @@ namespace COMP3304Session1
             //_notes[id].Add(text);
         }
 
+
+        /*
+
+         This should be done in 
+         _noteData
+
+
+         */
         /*
          * Adds 'FishyNote''s text into a dictionary
          * int id - id of the FishyNote
@@ -52,17 +74,20 @@ namespace COMP3304Session1
             //_notes[id].Text = text;
         }
 
+
+        /*
+
+         This should be done in 
+         _noteData
+
+
+         */
         /*
          * Removes text from the FishyNote
          */
         public void RemoveText(int id)
         {
             //_notes.Remove(id);
-        }
-
-        public void RemoveNote(int id)
-        {
-            _noteData.RemoveNote(id);
         }
     }
 }
